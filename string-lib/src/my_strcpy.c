7ed5149234bcfd7f4a9d0b2834e5ff6ead2ee170
Ranjan Kumar <111798933+ranjankumar555@users.noreply.github.com>
2025-02-14 16:31:52 +0530

    Create my_strcpy.c
    my_strcpy.c and my_strncpy.c completed

---
+++ string-lib/src/my_strcpy.c
@@ -0,0 +1,20 @@
+char* my_strcpy(char* dest, const char* src){
+  while(*src){
+    *dest = *src;
+    dest++;
+    src++;
+  }
+  *d = '/0';
+  return dest;
+}
+/* end of my_strcpy() */
+
+char* my_strncpy(char* dest, const char* src, unsigned int n){
+  int i=0;
+  while(i<n && src){
+    *dest++ = *src++;
+    i++;
+  }
+  return dest;
+}
+/* end of my_strncpy() */
