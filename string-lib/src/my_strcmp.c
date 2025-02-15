98f105bd65fa943033c986b2fd84f6f5c091a581
Ranjan Kumar <111798933+ranjankumar555@users.noreply.github.com>
2025-02-15 15:56:41 +0530

    Update my_strcmp.c

--- string-lib/src/my_strcmp.c
+++ string-lib/src/my_strcmp.c
@@ -1,8 +1,26 @@
-void my_strcmp(const char* str1, const char* str2){
-
+#include <stdio.h>
+int my_strcmp(const char* str1, const char* str2){
+  while(*str1){
+    if(*str1 != *str2){
+        return *str1 - *str2;
+    }
+    str1++;
+    str2++;
+  }
+  return *str1 - *str2;
 }
-void my_strncmp(const char* str1, const char* str2, unsigned int n){
 
+int my_strncmp(const char* str1, const char* str2, unsigned int n){
+  unsigned int i=0;
+  while(*str1 && i<n){
+    if(*str1 != *str2){
+        return *str1 - *str2;
+    }
+    str1++;
+    str2++;
+    i++;
+  }
+  return *str1 - *str2;
 }
 
 
