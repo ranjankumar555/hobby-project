f4689330eaaa09c656c0b73683c6a9cbc5b8bfc1
Ranjan Kumar <111798933+ranjankumar555@users.noreply.github.com>
2025-05-01 20:25:26 +0530

    Update my_strcat.c
    bug fixes: buffer overflow

--- string-lib/src/my_strcat.c
+++ string-lib/src/my_strcat.c
@@ -9,11 +9,11 @@ void my_strcat(char* str1, const char* str2){
 	
 	// copy charcter of string 2 in string 1.
 	while(*(str2+j)){
-		*(str+i) = *(str+j);
+		*(str1+i) = *(str2+j);
 		i++;
 		j++;
 	}
-	*(str+j) = '\0';
+	*(str1+i) = '\0';
 }
 void my_strncat(char* str1, const char* str2, unsigned int n){
 	int i=0, j=0;
@@ -25,10 +25,10 @@ void my_strncat(char* str1, const char* str2, unsigned int n){
 	
 	// copy n charcter of string 2 in string 1.
 	while(*(str2+j) && j<n){
-		*(str+i) = *(str+j);
+		*(str1+i) = *(str2+j);
 		i++;
 		j++;
 	}
-	*(str+j) = '\0';
+	*(str1+i) = '\0';
 }
 
